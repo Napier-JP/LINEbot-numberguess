4528d1ab301591a606a0558b9e20c03f0000bcbd
Napier-JP <26354173+Napier-JP@users.noreply.github.com>
2018-09-09 14:55:07 +0900

    response added

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-const path = require("path");
-const express = require("express");
-const line = require("@line/bot-sdk");
+const path = require("path"); //Node.jsの組み込みモジュール   require関数でパッケージを読み込む
+const express = require("express"); //Webappフレームワーク
+const line = require("@line/bot-sdk"); //LINEのbotSDK
 
 const lineConfig = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET
 };
-const lineClient = new line.Client(lineConfig);
+const lineClient = new line.Client(lineConfig);  //LINEサーバにリクエストを投げるクライアントを生成
 
 function createReplyMessage(input) {
-  // 1. 固定メッセージを返す
+  // 2. オウム返しする
   return {
     type: "text",
-    text: "pong"
+    // `（バッククォート）で囲った中で${変数名}や${式}を書くと結果が展開される
+    // テンプレートリテラル（Template literal）という文法です
+    text: "You said "+input+", loud and clear"
+    // 以下と同じです
+    // text: input + '、と言いましたね？'
   };
 }
 
-const server = express();
+const server = express(); //expressインスタンス生成
 
 server.use("/images", express.static(path.join(__dirname, "images")));
 
-server.post("/webhook", line.middleware(lineConfig), (req, res) => {
+server.post("/webhook", line.middleware(lineConfig), (req, res) => { //サーバからのリクエストを受ける
   // LINEのサーバーに200を返す
   res.sendStatus(200);
 
@@ -32,4 +36,4 @@ server.post("/webhook", line.middleware(lineConfig), (req, res) => {
   }
 });
 
-server.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 8080); //サーバ起動
